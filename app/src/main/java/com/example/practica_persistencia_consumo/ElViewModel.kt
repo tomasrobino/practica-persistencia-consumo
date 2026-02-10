1df4adbf53b6e0c8a496c85491b3eeeb6ed52ecf
Tomas Robino <tomaserobino@gmail.com>
2026-02-10 10:59:14 +0100

    flowstate

--- app/src/main/java/com/example/practica_persistencia_consumo/ElViewModel.kt
+++ app/src/main/java/com/example/practica_persistencia_consumo/ElViewModel.kt
@@ -2,10 +2,17 @@ package com.example.practica_persistencia_consumo
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ElViewModel(private val cocheDao: CocheDao): ViewModel() {
-    val allCoches = cocheDao.getAllCoches()
+    val allCoches: StateFlow<List<Coche>> = cocheDao.getAllCoches().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = emptyList()
+    )
 
     fun insertCoche(color: String, marca: String, modelo: String) {
         viewModelScope.launch {
