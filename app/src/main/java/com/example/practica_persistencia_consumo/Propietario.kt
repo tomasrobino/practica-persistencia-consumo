7e0f48de7524561c18530fbb0b93758ee2e455e0
Tomas Robino <tomaserobino@gmail.com>
2026-02-21 19:47:39 +0100

    propietario dao

--- app/src/main/java/com/example/practica_persistencia_consumo/Propietario.kt
+++ app/src/main/java/com/example/practica_persistencia_consumo/Propietario.kt
@@ -1,26 +1,55 @@
 package com.example.practica_persistencia_consumo
 
+import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
+import androidx.room.Query
 import androidx.room.Relation
+import androidx.room.Transaction
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Entity(tableName = "propietario")
-data class Propietario (
-
+data class Propietario(
     @PrimaryKey(autoGenerate = true) val propietarioId: Int = 0,
     val nombre: String,
     val telefono: String
-
 )
 
-
-//Clase que relaciona Propietarios con Coches
 data class PropietarioConCoches(
     @Embedded val propietario: Propietario,
     @Relation(
         parentColumn = "propietarioId",
-        entityColumn = "propietarioId" // El campo que añadimos en Coche
+        entityColumn = "propietarioId"
     )
     val coches: List<Coche>
 )
+
+@Dao
+interface PropietarioDao {
+    @Insert
+    suspend fun insertPropietario(propietario: Propietario): Long
+
+    @Update
+    suspend fun updatePropietario(propietario: Propietario)
+
+    @Delete
+    suspend fun deletePropietario(propietario: Propietario)
+
+    @Query("SELECT * FROM propietario")
+    fun getAllPropietarios(): Flow<List<Propietario>>
+
+    @Query("SELECT * FROM propietario WHERE propietarioId = :id")
+    fun getPropietarioById(id: Int): Flow<Propietario?>
+
+    @Transaction
+    @Query("SELECT * FROM propietario")
+    fun getPropietariosConCoches(): Flow<List<PropietarioConCoches>>
+
+    @Transaction
+    @Query("SELECT * FROM propietario WHERE propietarioId = :id")
+    fun getPropietarioConCochesById(id: Int): Flow<PropietarioConCoches?>
+}
