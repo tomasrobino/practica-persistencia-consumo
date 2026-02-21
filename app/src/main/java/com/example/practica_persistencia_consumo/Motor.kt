ee16160f91002bcf707fda075ae57b114a43a05e
Tomas Robino <tomaserobino@gmail.com>
2026-02-21 20:30:47 +0100

    on conflict strategy para motor

--- app/src/main/java/com/example/practica_persistencia_consumo/Motor.kt
+++ app/src/main/java/com/example/practica_persistencia_consumo/Motor.kt
@@ -1,33 +1,31 @@
 package com.example.practica_persistencia_consumo
+
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Entity(
-    tableName ="motores",
-    //Esta es la parate necesiara para no tener que creal el CRUD
-    // del resto de entidades que no sean Coche
+    tableName = "motores",
     foreignKeys = [
         ForeignKey(
             entity = Coche::class,
-            parentColumns = ["id"],      // El ID del Coche
-            childColumns = ["cocheId"],  // La columna en Motor que lo referencia
+            parentColumns = ["id"],
+            childColumns = ["cocheId"],
             onDelete = ForeignKey.CASCADE,
             onUpdate = ForeignKey.CASCADE
         )
     ],
-    // Para forzar que no se repitan los ids
     indices = [Index(value = ["cocheId"], unique = true)]
 )
-
-data class Motor (
+data class Motor(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val marca: String,
     val modelo: String,
@@ -37,7 +35,7 @@ data class Motor (
 
 @Dao
 interface MotorDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMotor(motor: Motor)
 
     @Query("SELECT * FROM motores")
